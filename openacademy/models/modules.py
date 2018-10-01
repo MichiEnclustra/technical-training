6aeb8d46c965b2a33d9af20e10b0f413f081de9e
Mike Zahno <info@smartforce.ch>
2018-10-01 14:09:25 +0200

    test2

--- openacademy/models/modules.py
+++ openacademy/models/modules.py
@@ -1,18 +1,19 @@
 from odoo import models, fields, api
 
+
 class Course(models.Model):
     _name = 'openacademy.course'
     _description = 'Inheritance Zero'
-    
+
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
     checkbox = fields.Boolean("Test Checkbox", default=True)
-    
+
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
 
     instructor_id = fields.Many2one('res.partner', string="Instructor")
     #course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
-    
-    
+
+    # test

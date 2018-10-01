311291678dc4b0bfc25d782ef11dd3e52a23e5ad
Mike Zahno <info@smartforce.ch>
2018-10-01 14:12:30 +0200

    add descr

--- openacademy/models.py
+++ openacademy/models.py
@@ -2,8 +2,10 @@
 
 from odoo import models, fields, api
 
+
 class Courses(models.Model):
     _name = 'openacademy.course'
+    _description = 'Courses'
 
     name = fields.Char()
     user_id = fields.Many2one('res.users', string="Responsible")
@@ -11,6 +13,7 @@ class Courses(models.Model):
 
 class Sessions(models.Model):
     _name = 'openacademy.session'
+    _description = 'Sessions'
 
     course_id = fields.Many2one('openacademy.course', string="Course")
     user_id = fields.Many2one('res.users', string="Instructor")

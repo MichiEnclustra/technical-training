d1ad7fefe7e7c960ecf6f746fcb4fc2c69332a49
Michi Zahno <mzahno@enclustra.com>
2018-10-01 14:35:56 +0200

    asd

--- openacademy/models.py
+++ openacademy/models.py
@@ -15,18 +15,18 @@ class Sessions(models.Model):
     _name = 'openacademy.session'
     _description = 'Sessions'
 
-    course_id = fields.Many2one('openacademy.course', string="Course")
-    user_id = fields.Many2one('res.users', string="Instructor")
-    start_date = fields.Date()
-    seats = fields.Integer('Room Capacity')
-    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True))
+    user_id=fields.Many2one('res.users', string = "Instructor")
+    start_date=fields.Date()
+    seats=fields.Integer('Room Capacity')
+    attendee_ids=fields.Many2many('res.partner', string = "Attendees")
 
 
-class ComputedModel(models.Model):
-    _name = 'test.computed'
-    name = fields.Char(compute='_compute_name')
+    class ComputedModel(models.Model):
+    _name='test.computed'
+    name=fields.Char(compute = '_compute_name')
 
     @api.multi
     def _compute_name(self):
-        for record in self:
-            record.name = str(random.randint(1, 1e6))
+    for record in self:
+        record.name=str(random.randint(1, 1e6))

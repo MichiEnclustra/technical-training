5336b0b76fa61e729559a2902f286d24e32536d1
Michi Zahno <mzahno@enclustra.com>
2018-10-01 14:22:57 +0200

    add computed field

--- openacademy/models.py
+++ openacademy/models.py
@@ -20,3 +20,14 @@ class Sessions(models.Model):
     start_date = fields.Date()
     seats = fields.Integer('Room Capacity')
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
+
+class ComputedModel(models.Model):
+    _name = 'test.computed'
+
+    name = fields.Char(compute='_compute_name')
+
+    @api.multi
+    def _compute_name(self):
+        for record in self:
+            record.name = str(random.randint(1, 1e6))
